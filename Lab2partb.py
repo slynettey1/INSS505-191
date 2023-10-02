e3703d31e4770e6db3d9bd971f0354fd6e61acdc
slynettey <netteys1212@students.bowiestate.edu>
2023-10-02 18:20:20 -0400

    Lab2files

--- Lab2partb.py
+++ Lab2partb.py
@@ -24,3 +24,12 @@ print(mult)
 print(subtract)
 print(division)
 print(add,mult,subtract,division)
+number=13
+number=12
+number3=11
+number4=10
+add=number1+number2+number3+number4
+mult=number1*number2*number3*number4
+subtract=number4-number3-number2-number1
+division=(number4/number3)
+print(add,mult,subtract,division)
