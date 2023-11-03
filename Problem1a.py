cf9f52df1b0a0ce2040469fd3c5e3312d27177ab
slynettey <netteys1212@students.bowiestate.edu>
2023-11-03 15:39:26 -0400

    Merge remote-tracking branch 'origin/main'

---
+++ Problem1a.py
@@ -0,0 +1,7 @@
+#collecting two inputs from users using the input function
+length=int(input("Enter the length of a rectangle:"))
+breadth=int(input("Enter the breadth of a rectangle:"))
+
+#The formula to calculate the area of a rectangle is:
+Area=length*breadth
+print(Area)
