ee486ab6619e4a72735afec5ae98a46faa6a0f72
slynettey <netteys1212@students.bowiestate.edu>
2023-11-03 16:02:33 -0400

    Merge remote-tracking branch 'origin/main'

---
+++ Problem3a.py
@@ -0,0 +1,12 @@
+#what is average
+#collect 5 inputs from users
+attempt1 =int(input("Enter attempt1:"))
+attempt2 =int(input("Enter attempt2:"))
+attempt3 =int(input("Enter attempt3:"))
+attempt4 =int(input("Enter attempt4:"))
+attempt5 =int(input("Enter attempt5:"))
+add=(attempt1+attempt2+attempt3+attempt4+attempt5)
+print(add)
+
+average = add/5
+print(int(average))
